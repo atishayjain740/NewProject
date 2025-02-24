0e1cf72f9f0fc5d2ff857e0c49a0c2148088e976
atishayjain740 <atishayjain740@gmail.com>
2025-02-23 17:41:55 -0800

    text change 1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World! Welcome to flutter New project'),
+          child: Text('Hello World! Welcome to flutter New project 1'),
         ),
       ),
     );
